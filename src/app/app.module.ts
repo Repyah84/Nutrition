d3e49ee5d9a51a944f1a6b79ab6ed1742b881866
Nikolay <repyah84@gmail.com>
2022-01-05 18:59:26 +0200

    add sass helpers (mixins,functions.variables)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NutritionHeaderComponent } from './components/nutrition-header/nutrition-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NutritionHeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/nutrition-header/nutrition-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nutrition-header',
+  templateUrl: './nutrition-header.component.html',
+  styleUrls: ['./nutrition-header.component.scss']
+})
+export class NutritionHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
