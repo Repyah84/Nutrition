9cd02bfd54b4cec7110efeb9d7a8efb8c2dcf8c2
Nikolay <repyah84@gmail.com>
2023-02-19 12:33:03 +0200

    refactor error page

--- src/app/pages/nutrition-404-page/nutrition-404-page.module.ts
+++ src/app/pages/nutrition-404-page/nutrition-404-page.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule, Routes } from '@angular/router';
 import { Nutrition404PageComponent } from './nutrition-404-page.component';
 
@@ -11,6 +13,6 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [Nutrition404PageComponent],
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes), MatButtonModule, MatIconModule],
 })
 export class Nutrition404PageModule {}
